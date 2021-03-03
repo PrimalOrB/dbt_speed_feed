b9ed8325e92414ed3261092346ee823347aae3b3
Andrew Ogilvie <primalorb@gmail.com>
2021-03-03 09:23:42 -0500

    initial working version

---
+++ js/calculation.js
@@ -0,0 +1,133 @@
+import { sfmTable, materialTable } from './tables.js';
+
+var input = {}
+var output = {}
+
+export function runCalculate(e) {
+return updateCalc(e)
+}
+
+function updateCalc(input) {
+    // find matching SFM table
+    var filterInput = sfmTable.filter(lookup => lookup.series == input.series && lookup.material == input.material)
+
+    // add inputs to filtered object
+    Object.assign(filterInput[0],input)
+
+    // add material selection data to filtered object
+    var materialType = materialTable.filter( lookup => lookup.material === input.material )
+    Object.assign(filterInput[0],materialType[0])
+
+
+
+    // filter for series table
+    filterInput[0].table = filterInput[0].table.filter(lookup => lookup.series == filterInput[0].series && lookup.material == filterInput[0].material)
+    // interpolate FPT values from low and high dia ranges
+    filterInput[0].fpt = parseFloat(interpolateFPT(filterInput[0])).toFixed(4)
+    // calculate outputs
+    output['effective-diameter'] = filterInput[0].dia;
+    output['sfm'] = interpolateSFM(filterInput[0]);
+    output['ipt'] = filterInput[0]['step-over'] >= ( filterInput[0].dia * 0.8 ) ? 
+                    parseFloat(filterInput[0].fpt * 0.8).toPrecision(2) : 
+                    parseFloat(filterInput[0].fpt).toPrecision(2);
+    output['rpm'] = filterInput[0]['max-rpm'] === undefined ?
+                    Math.round( output['sfm'] * 12 / Math.PI / filterInput[0].dia / 100 ) * 100 :
+                    Math.round( Math.min( filterInput[0]['max-rpm'], output['sfm'] * 12 / Math.PI / filterInput[0].dia) / 100 ) *100;
+    output['ipm'] = output['ipt'] * output['rpm'] * filterInput[0]['num-teeth'];
+    // apply chip thinning
+    chipThin(output, filterInput[0])
+    // continue after chip thinning update
+    output['removal'] = filterInput[0]['depth-of-cut'] * filterInput[0]['step-over'] * output['ipm'];
+    if(filterInput[0]['specific-energy'] ) { 
+        output['min-hp'] = parseFloat( ( output['removal'] * filterInput[0]['specific-energy'] ) / 0.75 ).toFixed(2);
+        output['spindle-tq']  = parseFloat( output['min-hp'] * 0.75 * 63030 / output['rpm'] ).toFixed(2);
+    };
+    return output
+}
+
+// Function to interpolate the feed per tooth (fpt) between key upper and lower diameter inputs
+function interpolateFPT(obj){
+    function lowMatch(obj) {
+        var val = obj.table.filter(lookup => lookup['cut-dia'] <= obj.dia)
+        val = val[val.length-1]
+        return val
+    }
+    function highMatch(obj) {
+        var val = obj.table.filter(lookup => lookup['cut-dia'] >= obj.dia)
+        val = val[0]
+        return val
+    } 
+    var a = lowMatch(obj)
+    var b = highMatch(obj)
+   
+    if ( a !== b ) { 
+        var ratio = (obj.dia - a['cut-dia']) / ( b['cut-dia'] - a['cut-dia'] )
+        return ( ( b.fpt - a.fpt ) * ratio ) + a.fpt
+    } else {
+        return a.fpt
+    }
+};
+
+// Function to interpolate SFM from tables
+function interpolateSFM(obj) {
+    var a = obj['min-sfm']
+    var b = obj['max-sfm']
+    var c = obj['aggression']
+    return ( ( b - a ) * ( c/10 ) ) + a
+};
+
+// Apply chip thinning calculations
+function chipThin(obj, input) {
+    obj['radial-chip-thinning-factor'] = Math.sqrt( 1 - Math.pow(1 - ( ( 2 * input['step-over'] ) / input['dia'] ), 2 ) );
+    obj['actual-chip-thickness'] = obj['ipt'] / obj['radial-chip-thinning-factor'];
+    switch(input['tool-type']) {
+        case 'FB': // FB Condiution
+            thinningFB(obj, input);
+            break
+        case 'BN': // BN Condition
+            thinningBN(obj, input);
+            break
+        case 'RAD': // RAD Condition
+            thinningRAD(obj, input);
+            break
+    }
+    return output
+};
+
+// FB Chip Thinning Calculations
+function thinningFB(obj, input) {
+    if( input['step-over'] < ( input['dia'] / 2 ) ) {
+        obj['ipt'] = parseFloat(obj['actual-chip-thickness']).toFixed(4)
+        obj['ipm'] = obj['ipm'] / obj['radial-chip-thinning-factor'];
+    }
+}
+
+// BN Chip Thinning Calculations
+function thinningBN(obj, input) {
+    if ( input['depth-of-cut'] < input['dia'] / 2 ) {
+        output['effective-diameter'] = 2 * Math.sqrt( Math.pow(input['dia'] / 2, 2) - Math.pow( ( input['dia'] / 2 ) - input['depth-of-cut'], 2 ) )
+    }
+    obj['axial-chip-thinning-factor'] = Math.sqrt( 1 - Math.pow( 1 - ( 2 * input['depth-of-cut'] ) / output['effective-diameter'], 2 ) );
+    obj['combined-chip-thin'] = obj['axial-chip-thinning-factor'] * obj['radial-chip-thinning-factor'];
+    if( obj['combined-chip-thin'] ) { 
+        obj['actual-chip-thickness'] = obj['ipt'] / obj['combined-chip-thin']
+    }
+    obj['ipt'] = obj['actual-chip-thickness'];
+    obj['rpm'] = Math.round( Math.min( obj['rpm'], obj['rpm'] * 12 / Math.PI / output['effective-diameter']) / 100 ) * 100;
+    obj['ipm'] = obj['ipt'] * obj['rpm'] * input['num-teeth']
+}
+
+// RAD Chip Thinning Calculations
+function thinningRAD(obj, input) {
+    input['radius'] === input['radius'] || 0;
+    if ( input['depth-of-cut'] < input['radius'] ) {
+        if( input['dia'] - ( 2 * input['radius'] ) <= 0 ) {
+        output['effective-diameter'] = 2 * Math.sqrt( Math.pow(input['radius'], 2) - Math.pow( input['radius'] - input['depth-of-cut'], 2 ) )
+        } else {
+            output['effective-diameter'] = 2 * Math.sqrt( Math.pow(input['radius'], 2) - Math.pow( input['radius'] - input['depth-of-cut'], 2 ) ) + ( input['dia'] - ( 2 * input['radius']))
+        }
+    }
+    obj['ipt'] = input.fpt / obj['radial-chip-thinning-factor'];
+    obj['rpm'] = Math.round( Math.min( obj['rpm'], obj['rpm'] * 12 / Math.PI / output['effective-diameter']) / 100 ) * 100;
+    obj['ipm'] = obj['ipt'] * obj['rpm'] * input['num-teeth']
+}
---
+++ js/input.js
@@ -0,0 +1,89 @@
+import { sfmTable } from './tables.js';
+import { runCalculate } from './calculation.js'
+
+// the tool inputs from DOM
+var toolInput = {}
+// the output object to send to the calculator
+var outputObj = {}
+
+// Listener for change to update the output data
+window.addEventListener('change', function(e) {
+    if(e.target.id === 'series') { // collect refresh material list and collect tool type
+        updateMaterials(e)
+        output(e)
+    } else if ( e.target.id === 'material' || e.target.id === 'type-end' ) { // collect drop down selections
+        outputObj[`${e.target.id}`] = e.target.selectedOptions[0].innerText
+        output(e)
+    }  else if( e.target.classList.value === 'collect' ) { // collect other input data
+        output(e)
+    }
+})
+
+// load the list of tool types into the dropdown list from the sfm table
+function init() {  
+    toolInput.series = [...new Set(sfmTable.map(x => x.series))]
+    initialToolList()
+}
+init();
+
+// load tool list to the DOM and run the updateMaterials to generate the list of materials available
+function initialToolList() {
+    var list = document.getElementById("series")
+    for ( var i = 0; i < toolInput.series.length; i++) {
+        list.insertAdjacentHTML('beforeend',`<option value="">${toolInput.series[i]}</a>`)
+    }
+    toolInput['series'] = toolInput.series[0]
+    var e = toolInput
+    updateMaterials(e)
+}
+
+// update the list of materials available from the tol type list
+function updateMaterials(e) {
+    // if function being called as a change, update the toolInput tool type, otherwise continue with current
+    if( e.type === 'change') {
+        toolInput[`${e.target.id}`] = e.target.selectedOptions[0].innerText
+    }
+    // send current tool type to output
+    outputObj['series'] = toolInput['series']
+    // call material list
+    materialList()
+}
+
+// load the list of available materials (by tool type) to the dropdown list
+function materialList() {
+    var e = toolInput['series']
+    var list = document.getElementById("material")
+    // clear current DOM dropdown list
+    list.innerHTML = "";
+    var matList = sfmTable.filter(lookup => lookup.series == e)
+    for ( var i = 0; i < matList.length; i++) {
+        list.insertAdjacentHTML('beforeend',`<option value="${matList[i].material}">${matList[i].material}</a>`)
+    }
+    // send material type to output
+    outputObj['material'] = document.getElementById('material').value
+}
+
+// populate output object
+function output(e) {
+    // send collect class data to output
+    var data = document.querySelectorAll('.collect')
+    for( var i = 0 ; i < data.length ; i++) {
+        // if data is number, store as number
+        if( isNaN( parseFloat( data[i].value ) ) ) {
+            outputObj[`${data[i].id}`] = data[i].value
+        } else {
+            outputObj[`${data[i].id}`] = parseFloat( data[i].value )
+        }
+    }
+    post( runCalculate(outputObj) )
+}
+
+function post(e) {
+    var postDOM = document.getElementById('post')
+    postDOM.innerHTML = "";
+    for ( const [key, value] of Object.entries(e) ) {
+        postDOM.insertAdjacentHTML('beforeend',`<li>${key} - ${value}</li>`)
+        // console.log(e)
+    }
+}
+
